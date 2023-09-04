1a73e79ac470e0e8344004e27b40e00cf3367ba1
enpitsulin <enpitsulin@gmail.com>
2023-09-04 16:18:16 +0800

    feat: 未添加账号时的popup

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -18,6 +18,10 @@ watch(uid, (uid, oldUid) => {
   if (uid !== '' && cred.value !== '' && uid !== oldUid)
     execute()
 }, { immediate: true })
+
+function openOptionsPage() {
+  browser.runtime.openOptionsPage()
+}
 </script>
 
 <template>
@@ -26,6 +30,36 @@ watch(uid, (uid, oldUid) => {
     bg="[url(/assets/popup-bg.jpg)] cover center-bottom"
     c-foreground p-2 w-350px
   >
+    <div v-if="arknightsBinding.length === 0">
+      <div py-4>
+        <h2 text-xl>
+          暂无账号
+        </h2>
+        <span>No Account</span>
+      </div>
+      <button
+        outline-none relative flex="~ items-center"
+        p="x2 y1"
+        c="#ababab hover:primary"
+        class="group"
+        @click="openOptionsPage"
+      >
+        <div
+          absolute top-0 left-0
+          h="20% group-hover:50%" w-full
+          border="~ #ababab b-0 group-hover:primary"
+          transition="all duration-150"
+        />
+        <span text-sm>前去添加</span>
+        <svg stroke="#ababab" w-16 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 18"><path d="M6 13 h35 l-6 -6" fill="none" /></svg>
+        <div
+          absolute bottom-0 left-0
+          h="20% group-hover:50%" w-full
+          border="~ #ababab t-0 group-hover:primary"
+          transition="all duration-150"
+        />
+      </button>
+    </div>
     <div v-if="userInfo">
       <CharacterSwitcher
         v-model:uid="uid"
@@ -40,7 +74,7 @@ watch(uid, (uid, oldUid) => {
         :tower="userInfo.data.tower"
         :routine="userInfo.data.routine"
       />
+      <PopupFooter />
     </div>
-    <PopupFooter />
   </main>
 </template>
