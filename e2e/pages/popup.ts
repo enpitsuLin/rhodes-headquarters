3f49f13eda9c035d59287c7e80bb9fc50b8e1c75
enpitsulin <enpitsulin@gmail.com>
2024-04-16 15:31:32 +0800

    test: fix e2e selector

--- e2e/pages/popup.ts
+++ e2e/pages/popup.ts
@@ -3,7 +3,7 @@ import type { Page } from '@playwright/test'
 export async function openPopup(page: Page, extensionId: string) {
   await page.goto(`chrome-extension://${extensionId}/popup.html`)
 
-  await page.waitForSelector('#counter')
+  await page.waitForSelector('main.skland-container')
 
   // TODO
   return true
