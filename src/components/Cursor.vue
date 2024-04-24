56b9adb29acfc4b70500aa96538a756d2ce95592
enpitsulin <enpitsulin@gmail.com>
2024-04-24 21:17:15 +0800

    feat: cursor size larger

--- src/components/Cursor.vue
+++ src/components/Cursor.vue
@@ -15,7 +15,7 @@ const {
       ref="outer"
       z-10000
       fixed pointer-events-none
-      h-24px w-24px
+      h-28px w-28px
       border="1px solid foreground rounded-full"
       class="[transform:translate3d(-50%,-50%,0px)] [transition:background-color_0.3s,width_0.3s,height_0.3s]"
     />
