f94fd1f1c444e7a6dd7fc8e81be2707d71a11509
enpitsulin <enpitsulin@gmail.com>
2024-04-25 19:10:57 +0800

    feat: preferences periodInMinutes

--- src/entrypoints/background/index.ts
+++ src/entrypoints/background/index.ts
@@ -1,4 +1,5 @@
 /// <reference lib="webworker"/>
+import { preferenceStorage } from '@/store/preference'
 import { registerAccountService } from '@/utils/proxy-service'
 
 export default defineBackground({
@@ -7,6 +8,11 @@ export default defineBackground({
     const accountService = registerAccountService()
 
     accountService.createRefreshInfoAlarm()
+
+    preferenceStorage.watch((value, oldValue) => {
+      if (oldValue.periodInMinutes !== value.periodInMinutes)
+        accountService.createRefreshInfoAlarm()
+    })
   },
 })
 
--- src/pages/preferences.vue
+++ src/pages/preferences.vue
@@ -5,6 +5,14 @@ import { preferenceStorage } from '@/store/preference'
 import OptionLayout from '~/components/layouts/options.vue'
 
 const preferences = useWxtStorage(preferenceStorage)
+
+function onChange(e: Event) {
+  const periodInMinutes = Number((e.target as HTMLInputElement).value)
+
+  preferences.value.periodInMinutes = periodInMinutes === 0
+    ? preferenceStorage.defaultValue.periodInMinutes
+    : periodInMinutes
+}
 </script>
 
 <template>
@@ -17,15 +25,16 @@ const preferences = useWxtStorage(preferenceStorage)
       <li>
         <PreferenceItem title="刷新时间" description="定时刷新游戏角色最新数据的时间，单位分钟，默认 10 分钟。">
           <input
-            v-model="preferences.periodInMinutes"
-            type="text"
+            :placeholder="preferences.periodInMinutes.toString()"
+            type="input" inputmode="numeric"
             bg="transparent" w-36px h-30px
             border="~ border focus:primary rounded-sm" px-2 py-2 outline-none
+            @change="onChange"
           >
         </PreferenceItem>
       </li>
       <li>
-        <PreferenceItem title="多账户提醒" description="对非当前账户的其余账户信息提供可用提醒，如理智即将恢复，公招即将结束等。">
+        <PreferenceItem title="多账户提醒(WIP)" description="对非当前账户的其余账户信息提供可用提醒，如理智即将恢复，公招即将结束等。">
           <Switch v-model="preferences.charactersAlarmsEnable" />
         </PreferenceItem>
       </li>
