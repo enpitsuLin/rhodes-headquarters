77afa7607dd8c66114f4331a5a623ece9a003522
enpitsulin <enpitsulin@gmail.com>
2024-04-25 20:25:12 +0800

    feat: no account popup

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router/auto'
-import { useMagicKeys } from '@vueuse/core'
+import { useMagicKeys, useStyleTag } from '@vueuse/core'
 import { useAccounts, useCurrentAccount } from '@/composables/account'
 import LayoutDefault from '~/components/layouts/default.vue'
 import SectionSanity from '~/components/home/SectionSanity.vue'
@@ -15,7 +15,17 @@ const router = useRouter()
 const accounts = useAccounts()
 const currentAccount = useCurrentAccount()
 
+const { load, unload } = useStyleTag(`html,body{height:unset}#app {height:250px !important}`, { immediate: false })
+
 const characterInfo = useWxtStorage(chararcterStorage)
+
+watch(accounts, ({ length }) => {
+  if (length === 0)
+    load()
+
+  else
+    unload()
+}, { immediate: true })
 
 const { meta, control } = useMagicKeys()
 
@@ -33,10 +43,17 @@ function onOptionClick() {
 <template>
   <LayoutDefault>
     <template v-if="accounts.length === 0">
-      <h2>
-        TODO
-      </h2>
-      <div absolute bottom-6 left-0 right-0 flex="~ justify-center">
+      <div py-4>
+        <h2 text-xl>
+          暂无账号
+        </h2>
+        <span>No Account</span>
+      </div>
+      <p text-sm>
+        你需要点击下面的<code mx-1 px-2 rounded bg-secondary-background>+</code>去设置页添加你的森空岛账户来获取你的明日方舟角色信息
+      </p>
+
+      <div absolute bottom-0 h-60px bg="black/50" left-0 right-0 flex="~ justify-center">
         <Loading as="button" type="button" @click="onOptionClick">
           <svg size-20px c-primary width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M14 14H4V18H14V28H18V18H28V14H18V4H14V14Z" fill="currentColor" />
