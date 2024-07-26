db86f76af586e011479e362259a0b2714485bc11
enpitsulin <enpitsulin@gmail.com>
2024-07-26 17:57:45 +0800

    refactor: AddAccountDialog

--- src/components/dialog/AddAccountDialog.vue
+++ src/components/dialog/AddAccountDialog.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
+import { DialogBackdrop, DialogContent, DialogPositioner, DialogRoot, DialogTitle, Presence } from '@ark-ui/vue'
 import { useAsyncState } from '@vueuse/core'
-import { DialogBackdrop, DialogRoot } from '@ark-ui/vue'
-import { useDialog } from '@/composables/use-dialog'
-import { usePresence } from '@/composables/use-presence'
 
 const open = defineModel<boolean>('open', { required: true })
-
-const nodeRef = ref<HTMLDivElement | null>(null)
 
 const accountService = getAccountService()
 
@@ -14,30 +10,6 @@ const toast = useToast()
 
 const token = ref('')
 const errorMessage = ref('')
-
-const api = useDialog({
-  open,
-  onOpenChange(to) {
-    open.value = to
-  },
-})
-
-const presenceApi = usePresence({
-  present: computed(() => api.value.open),
-  onExitComplete: () => {
-    open.value = false
-    errorMessage.value = ''
-    token.value = ''
-  },
-})
-
-watch(nodeRef, () => {
-  if (nodeRef.value) {
-    const node = nodeRef.value
-    if (node)
-      presenceApi.value.setNode(node)
-  }
-})
 
 const { isLoading, execute } = useAsyncState(
   async () => {
@@ -69,84 +41,82 @@ const { isLoading, execute } = useAsyncState(
 </script>
 
 <template>
-  <DialogRoot>
+  <DialogRoot v-model:open="open">
     <Teleport to="body">
-      <div v-if="presenceApi.present" fixed inset-0>
-        <DialogBackdrop
-          fixed inset-0 z-100 bg-border:10 backdrop-blur-3
-          class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in data-[state=closed]:fade-out"
-        />
-        <div v-bind="api.getPositionerProps()" fixed inset-0 z-200 flex="~ items-center justify-center">
-          <div
-            ref="nodeRef" v-bind="api.getContentProps()" shadow="lg" flex="~ col" relative w-320px bg-background
-            class="data-[state=closed]:duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-90 data-[state=closed]:fade-out data-[state=open]:slide-in-from-bottom-10"
-          >
-            <header v-bind="api.getTitleProps()" flex="~ items-end" border="l-5px primary" relative h-50px select-none>
-              <div flex="~ items-baseline" pb-1 pl-4>
-                <div flex="~ items-center">
-                  <svg c-border width="6" height="18" viewBox="0 0 6 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_256_438)">
-                      <path d="M6 3V0H0V17.991V18H6V15H3V3H6Z" fill="currentColor" />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_256_438">
-                        <rect width="6" height="18" fill="white" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  <span text-base font-bold>新增账户</span>
-                  <svg c-border width="6" height="18" viewBox="0 0 6 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_256_441)">
-                      <path d="M0 3V0H6V17.991V18H0V15H3V3H0Z" fill="currentColor" />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_256_441">
-                        <rect width="6" height="18" fill="white" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </div>
-                <span text-base c-border font-bold>
-                  ACCOUNT
-                </span>
+      <DialogBackdrop
+        fixed inset-0 z-100 bg-border:10 backdrop-blur-3
+        class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in data-[state=closed]:fade-out"
+      />
+      <DialogPositioner fixed inset-0 z-200 flex="~ items-center justify-center">
+        <DialogContent
+          shadow="lg" relative w-320px bg-background
+          class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-90 data-[state=closed]:fade-out data-[state=open]:slide-in-from-bottom-10"
+        >
+          <DialogTitle flex="~ items-end" border="l-5px primary" relative h-50px select-none>
+            <div flex="~ items-baseline" pb-1 pl-4>
+              <div flex="~ items-center">
+                <svg c-border width="6" height="18" viewBox="0 0 6 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_256_438)">
+                    <path d="M6 3V0H0V17.991V18H6V15H3V3H6Z" fill="currentColor" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_256_438">
+                      <rect width="6" height="18" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                <span text-base font-bold>新增账户</span>
+                <svg c-border width="6" height="18" viewBox="0 0 6 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_256_441)">
+                    <path d="M0 3V0H6V17.991V18H0V15H3V3H0Z" fill="currentColor" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_256_441">
+                      <rect width="6" height="18" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
               </div>
-              <div absolute bottom-1px left-16px h-1px w-300px bg-border>
-                <div absolute size-3px left="-1px" bottom="-1px" bg-border />
-              </div>
-            </header>
-            <main>
-              <div p-4 space-y-2>
-                <p>1. 打开森空岛网页版并登录</p>
-                <p>
-                  2. 登录森空岛网页版后，打开 <a
-                    href="https://web-api.skland.com/account/info/hg"
-                    target="_blank"
-                  >https://web-api.skland.com/account/info/hg</a> 记下 content 字段的值
+              <span text-base c-border font-bold>
+                ACCOUNT
+              </span>
+            </div>
+            <div absolute bottom-1px left-16px h-1px w-300px bg-border>
+              <div absolute size-3px left="-1px" bottom="-1px" bg-border />
+            </div>
+          </DialogTitle>
+          <main>
+            <div p-4 space-y-2>
+              <p>1. 打开森空岛网页版并登录</p>
+              <p>
+                2. 登录森空岛网页版后，打开 <a
+                  href="https://web-api.skland.com/account/info/hg"
+                  target="_blank"
+                >https://web-api.skland.com/account/info/hg</a> 记下 content 字段的值
+              </p>
+              <p>3. 在下面输入获取到的值</p>
+              <div flex="~" relative>
+                <input
+                  v-model="token" type="text" border="~ border [&.warning]:red focus:primary" p="x-3 y2" flex-1
+                  bg-background outline-none :class="!!errorMessage && 'warning animate-shake'"
+                  @focus="errorMessage = ''"
+                >
+                <p absolute text-xs c-red bottom="-4.5">
+                  {{ errorMessage }}
                 </p>
-                <p>3. 在下面输入获取到的值</p>
-                <div flex="~" relative>
-                  <input
-                    v-model="token" type="text" border="~ border [&.warning]:red focus:primary" p="x-3 y2" flex-1
-                    bg-background outline-none :class="!!errorMessage && 'warning animate-shake'"
-                    @focus="errorMessage = ''"
-                  >
-                  <p absolute text-xs c-red bottom="-4.5">
-                    {{ errorMessage }}
-                  </p>
-                </div>
               </div>
-            </main>
-            <footer p="t-5px b-13px" flex="~ justify-center">
-              <button
-                :disabled="isLoading" h-32px w-250px p-10px bg="[url(~/assets/btn-bg.svg)]"
-                flex="inline justify-center items-center" @click="execute()"
-              >
-                {{ isLoading ? 'Loading...' : '新增账户' }}
-              </button>
-            </footer>
-          </div>
-        </div>
-      </div>
+            </div>
+          </main>
+          <footer p="t-5px b-13px" flex="~ justify-center">
+            <button
+              :disabled="isLoading" h-32px w-250px p-10px bg="[url(~/assets/btn-bg.svg)]"
+              flex="inline justify-center items-center" @click="execute()"
+            >
+              {{ isLoading ? 'Loading...' : '新增账户' }}
+            </button>
+          </footer>
+        </DialogContent>
+      </DialogPositioner>
     </Teleport>
   </DialogRoot>
 </template>
