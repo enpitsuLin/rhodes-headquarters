7fc0467e6b7c3934914224c07528192c07e10d4e
enpitsulin <enpitsulin@gmail.com>
2024-12-29 22:42:24 +0800

    fix: 属性对齐方式

--- src/layouts/options.vue
+++ src/layouts/options.vue
@@ -32,7 +32,10 @@ onBeforeRouteLeave((_to, _from, next) => {
 
 <template>
   <div bg="background" relative h-full w-full>
-    <header relative h-160px w-full select-none of-hidden class="from-#0C4D78:0 to-#0C4D78:80 bg-gradient-linear">
+    <header
+      relative h-160px w-full select-none of-hidden
+      class="from-#0C4D78:0 to-#0C4D78:80 bg-gradient-linear"
+    >
       <div v-if="showBack" pt-3>
         <button @click="$router.go(-1)">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
@@ -41,12 +44,19 @@ onBeforeRouteLeave((_to, _from, next) => {
         </button>
       </div>
       <div absolute left="-20px" bottom="-20px">
-        <span text-nowrap text-65px font-bold tracking="-11%" c="background">
+        <span
+          text-nowrap text-65px font-bold
+          tracking="-11%" c="background"
+        >
           {{ route.meta.backgroundTitle }}
         </span>
       </div>
 
-      <div absolute left-0 top-71px h-70px border="l-4px primary" p="l-11px t-9px">
+      <div
+        absolute left-0 top-71px h-70px
+        border="l-4px primary"
+        p="l-11px t-9px"
+      >
         <div :key="route.path" flex items-center view-transition-navbar-title>
           <svg text-primary width="10" height="32" viewBox="0 0 10 32" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M10 0H0V32H10V26.5H4.7619V5H10V0Z" fill="currentColor" />
@@ -67,7 +77,11 @@ onBeforeRouteLeave((_to, _from, next) => {
         </svg>
       </div>
     </header>
-    <main absolute left-15px right-15px top-145px :class="route.meta.contentClass">
+    <main
+      absolute
+      left-15px right-15px top-145px
+      :class="route.meta.contentClass"
+    >
       <RouterView />
     </main>
     <RouterView name="fab" />
@@ -75,15 +89,17 @@ onBeforeRouteLeave((_to, _from, next) => {
 
   <nav v-if="!showBack" absolute left-0 right-0 flex="~" border="t-1 border/30">
     <RouterLink
-      to="/options" role="button" w="1/2"
-      relative block bg-background:60 py-3 text-center font-bold
+      to="/options" role="button"
+      w="1/2" relative block bg-background:60
+      py-3 text-center font-bold
       active-class="active after:(content-empty absolute bottom-0 inset-x-0 h-3px bg-primary view-transition-navbar-active)"
     >
       账号管理
     </RouterLink>
     <RouterLink
-      to="/preferences" role="button" w="1/2"
-      relative block bg-background:60 py-3 text-center font-bold
+      to="/preferences" role="button"
+      w="1/2" relative block bg-background:60
+      py-3 text-center font-bold
       active-class="active after:(content-empty absolute bottom-0 inset-x-0 h-3px bg-primary view-transition-navbar-active)"
     >
       系统设置
