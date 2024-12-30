dc21a428eef8b1b896ff3c5bdfbd77437948f7c1
enpitsulin <enpitsulin@gmail.com>
2024-12-30 21:03:55 +0800

    feat: 基建入口

--- src/components/home/SectionTitle.vue
+++ src/components/home/SectionTitle.vue
@@ -6,9 +6,12 @@ defineProps<{ title: string, sub: string }>()
   <h3
     h-30px w-320px class="bg-[url(~/assets/bg-section-title.svg)]"
     p="l-32px y-3px"
-    flex="inline items-center gap-1"
+    flex="inline items-center justify-between"
   >
-    <span text-base font-bold>{{ title }}</span>
-    <span text-xs op-80>{{ sub }}</span>
+    <div flex="inline items-center gap-1">
+      <span text-base font-bold>{{ title }}</span>
+      <span text-xs op-80>{{ sub }}</span>
+    </div>
+    <slot />
   </h3>
 </template>
