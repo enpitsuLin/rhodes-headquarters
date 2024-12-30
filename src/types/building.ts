bd51d76f971f8f0be7a857e8547acf32a4663064
enpitsulin <enpitsulin@gmail.com>
2024-12-30 20:16:10 +0800

    docs: 为一些基建类型定义添加说明

--- src/types/building.ts
+++ src/types/building.ts
@@ -22,9 +22,13 @@ export interface ResidentCharacters {
  * 基建设施
  */
 export interface BuildingRoom {
+  /** 槽位 ID */
   slotId: string
+  /** 槽位状态 */
   slotState?: number
+  /** 干员 */
   chars: ResidentCharacters[]
+  /** 等级 */
   level: number
 }
 
@@ -50,6 +54,7 @@ export interface BuildingControl extends BuildingRoom {
  * 基建宿舍
  */
 export interface BuildingDormitory extends BuildingRoom {
+  /** 氛围值 */
   comfort: number
 }
 
@@ -57,8 +62,11 @@ export interface BuildingDormitory extends BuildingRoom {
  * 基建人力办公室
  */
 export interface BuildingHire extends BuildingRoom {
+  /** 状态 */
   state: number
+  /** 刷新次数 */
   refreshCount: number
+  /** 完成工作时间 */
   completeWorkTime: number
 }
 
@@ -82,12 +90,28 @@ export interface BuildingManufacture extends BuildingRoom {
  */
 export interface BuildingMeeting extends BuildingRoom {
   clue: {
-    board: ('RHINE' | 'PENGUIN' | 'BLACKSTEEL' | 'URSUS' | 'KJERAG' | 'RHODES' | 'GLASGOW')[]
+    /**
+     * 线索板
+     * - 1. RHINE: 莱茵生命
+     * - 2. PENGUIN: 企鹅物流
+     * - 3. BLACKSTEEL: 黑钢国际
+     * - 4. URSUS: 乌萨斯学生自治团
+     * - 5. GLASGOW: 格拉斯哥帮
+     * - 6. KJERAG: 喀兰学院
+     * - 7. RHODES: 罗德岛
+     */
+    board: ('RHINE' | 'PENGUIN' | 'BLACKSTEEL' | 'URSUS' | 'GLASGOW' | 'KJERAG' | 'RHODES')[]
+    /** 已拥有线索数量 */
     own: number
+    /** 已领取线索 */
     received: number
+    /** 已领取每日线索获取 */
     dailyReward: boolean
+    /** 传递线索数量 */
     needReceive: number
+    /** 线索交流结束时间 */
     shareCompleteTime: number
+    /** 线索交流中 */
     sharing: boolean
   }
   lastUpdateTime: number
@@ -104,6 +128,7 @@ export interface BuildingPower extends BuildingRoom {
  * 基建贸易站
  */
 export interface BuildingTrading extends BuildingRoom {
+  /** 订单 */
   stock: {
     delivery: {
       id: number
@@ -112,14 +137,31 @@ export interface BuildingTrading extends BuildingRoom {
     gain: {
       id: number
       count: number
+      /**
+       * 交付报酬
+       * - GOLD: 龙门币
+       * - DIAMOND: 合成玉
+       */
       type: 'GOLD' | 'DIAMOND'
     }
     instId: number
+    /**
+     * 订单需求
+     * - O_GOLD: 赤金
+     * - O_DIAMOND: 原石碎片
+     */
     type: 'O_GOLD' | 'O_DIAMOND'
   }[]
   stockLimit: number
+  /**
+   * 谈判策略
+   * - O_GOLD: 赤金——龙门商法
+   * - O_DIAMOND: 原石碎片——开采协力
+   */
   strategy: 'O_GOLD' | 'O_DIAMOND'
+  /** 订单完成时间 */
   completeWorkTime: number
+  /** 最后更新时间 */
   lastUpdateTime: number
 }
 
@@ -127,8 +169,10 @@ export interface BuildingTrading extends BuildingRoom {
  * 基建训练室
  */
 export interface BuildingTraining extends Omit<BuildingRoom, 'chars'> {
-  trainee?: Omit<ResidentCharacters, 'index' | 'bubble' | 'workTime'> & { targetSkill: number }
-  trainer?: Omit<ResidentCharacters, 'index' | 'bubble' | 'workTime'>
+  /** 训练干员 */
+  trainee: Omit<ResidentCharacters, 'index' | 'bubble' | 'workTime'> & { targetSkill: number } | null
+  /** 协助位干员 */
+  trainer: Omit<ResidentCharacters, 'index' | 'bubble' | 'workTime'> | null
   remainPoint: number
   speed: number
   lastUpdateTime: number
@@ -137,19 +181,30 @@ export interface BuildingTraining extends Omit<BuildingRoom, 'chars'> {
 
 /** 基建建筑 */
 export interface Building {
-  tiredChars: ResidentCharacters[]
-  powers: BuildingPower[]
-  manufactures: BuildingManufacture[]
-  tradings: BuildingTrading[]
-  dormitories: BuildingDormitory[]
-  meeting: BuildingMeeting
-  hire: BuildingHire
-  training: BuildingTraining
-  labor: Labor
+  /** 控制中枢 */
   control: BuildingControl
+  /** 发电站 */
+  powers: BuildingPower[]
+  /** 制造站 */
+  manufactures: BuildingManufacture[]
+  /** 贸易站 */
+  tradings: BuildingTrading[]
+  /** 宿舍 */
+  dormitories: BuildingDormitory[]
+  /** 人力办公室 */
+  hire: BuildingHire
+  /** 训练室 */
+  training: BuildingTraining
+  /** 会客室 */
+  meeting: BuildingMeeting
+  /** 无人机 */
+  labor: Labor
   /** 电梯 */
-  elevators: { slotId: string, slotState: number, level: 1 }[]
+  elevators: Omit<BuildingRoom, 'chars'>[]
   /** 走廊 */
-  corridors: { slotId: string, slotState: number, level: 1 }[]
+  corridors: Omit<BuildingRoom, 'chars'>[]
+  /** 家具 */
   furniture: { total: number }
+  /** 疲劳干员 */
+  tiredChars: ResidentCharacters[]
 }
