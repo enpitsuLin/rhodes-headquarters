fa7498c223537db562db2be7e88fbb8535787328
enpitsulin <enpitsulin@gmail.com>
2025-01-03 18:03:39 +0800

    feat: 建筑先固定排序

--- src/components/building/List.vue
+++ src/components/building/List.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import type { BindingInfo, Building } from '~/types'
+import type { BindingInfo } from '~/types'
 
-import Manufacture from '~/components/building/Manufacture.vue'
-import Trading from '~/components/building/Trading.vue'
-import Power from '~/components/building/Power.vue'
-import Dormitory from '~/components/building/Dormitory.vue'
 import Control from '~/components/building/Control.vue'
+import Dormitory from '~/components/building/Dormitory.vue'
 import Hire from '~/components/building/Hire.vue'
-import Training from '~/components/building/Training.vue'
+import Manufacture from '~/components/building/Manufacture.vue'
 import Meeting from '~/components/building/Meeting.vue'
+import Power from '~/components/building/Power.vue'
+import Trading from '~/components/building/Trading.vue'
+import Training from '~/components/building/Training.vue'
 
 const props = defineProps<{ info: BindingInfo }>()
 
-const buildingsType = ['control', 'powers', 'manufactures', 'tradings', 'dormitories', 'hire', 'training', 'meeting']
+const buildingsType = ['manufactures', 'tradings', 'meeting', 'dormitories', 'hire', 'training', 'powers', 'control']
 
 const nameComponentMap = {
   control: Control,
@@ -33,6 +33,11 @@ const list = computed(() => {
       return items.map(item => ({ type, data: item })) as { type: keyof typeof nameComponentMap, data: any }[]
     })
     .flat()
+    .sort((a, b) => {
+      const aType = a.type
+      const bType = b.type
+      return buildingsType.indexOf(aType) - buildingsType.indexOf(bType)
+    })
     .map(({ type, data }) => {
       const Component = nameComponentMap[type]
       // @ts-expect-error: ignore
